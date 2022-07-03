cae6a59144de593de91eb4b4b6aecfbbee771398
Joseleno Dias Moreira dos Santos <joselenomoreira@gmail.com>
2022-07-03 09:49:12 -0400

    first version

---
+++ src/components/Post/post.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Post = () => {
+  return (
+    <div id="post">
+      <div id="titre">
+        <img src="https://www.pinterest.com/pin/133489576434388177/" alt="" />
+        <p>UserName</p>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+        voluptatem fugiat suscipit laborum pariatur debitis nihil consequuntur
+        voluptas, id totam nesciunt animi ullam hic alias commodi asperiores?
+        Culpa, deleniti unde!
+      </p>
+      <div id="img"></div>
+    </div>
+  );
+};
